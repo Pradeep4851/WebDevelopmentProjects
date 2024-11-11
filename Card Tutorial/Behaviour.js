1d44e68a18fa7d253a8767c7ec689414613fdba5
Pradeep4851 <mallelapradeep4851@gmail.com>
2024-11-11 15:52:37 +0530

    initial commit on #CardTutorial

---
+++ Card Tutorial/Behaviour.js
@@ -0,0 +1,10 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', openNewTab);
+
+// function openNewTab(){
+//     alert('i am clicked');
+//     const href = document.querySelector("a").getAttribute("href");
+
+//     window.open(href, "_blank");
+// }
